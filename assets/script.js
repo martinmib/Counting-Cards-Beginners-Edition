33f437a2f8fe636027ea113eab36f7b5450ac8d0
Martin Brilly <martin_brilly@yahoo.co.uk>
2022-06-27 11:39:27 +0000

    add jscript functions tags

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+// wait for the 
+function runGame() {
+
+}
+
+function currentCount() {
+
+}
+function trueCount() {
+
+}
+
+function displayDivisionOption() {
+
+}
+
+function displaySubtractOption() {
+
+}
+function displayAdditionOption() {
+
+}
+
+function resetButton() {
+
+}
