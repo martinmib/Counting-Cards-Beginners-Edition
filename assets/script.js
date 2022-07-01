2b0762c6b6f61761caece61de2bcf5a9b028df3f
Martin Brilly <martin_brilly@yahoo.co.uk>
2022-07-01 12:31:03 +0000

    adding script

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
-// wait for the 
+// wait for the DOM to finish loading before running the game
+// get the button elements and add event listeners to them 
+document.addEventListener("DOMContentLoaded", function() {
+let buttons = this.getElementsByTagName("button");
+
+for (let button of buttons) { 
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "reset"){
+            alert("You clicked Reset!");
+        } else { 
+            let gameType = this.getAttribute("data-type");
+            alert(`You Clicked ${gameType}`)
+
+        }
+
+})
+}
+})
 function runGame() {
 
 }
